627326749f62c0cdefc0b14bd66a0777c853b6bc
Augusto Penaquione Zanetti <sasa>
2017-11-29 09:56:17 -0200

    initia commit

---
+++ src/app/app.routing.ts
@@ -0,0 +1,19 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './components/home/home.component';
+import { ListCoursesComponent } from './components/list-courses/list-courses.component';
+import { CourseDetailsComponent } from './components/course-details/course-details.component';
+
+const appRoutes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'courses', component: ListCoursesComponent},
+    {path: 'courses/detail/:tag', component: CourseDetailsComponent}
+]
+
+
+export const appRoutingProviders: any[] =[
+
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
